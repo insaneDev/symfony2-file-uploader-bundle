ffd11aa73b387e40f712d5c1a5dc890f8c613e11
Oscar onl. Nevarez Leal <onl@foreach.be>
2015-08-01 00:05:38 +0200

    Start UnitTest

---
+++ Tests/Test.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Nevarez
+ * Date: 8/1/15
+ * Time: 12:00 AM
+ */
+class Test extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     *
+     */
+    public function testFileUploader()
+    {
+        return $this->assertTrue(true);
+    }
+}
